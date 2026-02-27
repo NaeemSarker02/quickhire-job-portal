855f633abcdf40aa891e58619f6f94e4f4daf544
NAEEM SARKER <naeemsarker202@gmail.com>
2026-02-28 00:33:17 +0600

    Fixing repo sync and pushing code

--- quickhire-server/src/routes/applicationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitApplication } = require("../controllers/applicationController");
-const { validateApplication } = require("../middleware/validate");
-
-router.post("/", validateApplication, submitApplication);
-
-module.exports = router;
